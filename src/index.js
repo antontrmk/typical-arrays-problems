4fd1ec24b1f5de7626517bace867b74dd4715f06
antontrmk <antontrmk@gmail.com>
2021-03-12 04:54:12 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    array.sort((a,b)=>a-b);
+    return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    array.sort((a,b)=>a-b).reverse();
+    return array[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    array.forEach((item, index, array) => {
+      sum+=item;   
+    });
+    return sum/array.length;
+  }
 }
